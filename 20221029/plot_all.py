4e0bd1a4b07058e1cccf09aa390d84f3dc9095c5
Chigullapalli <anshukc2@illinois.edu>
2022-11-01 00:56:21 -0500

    Mach Number and Temperature Analysis

--- 20221029/plot_all.py
+++ 20221029/plot_all.py
@@ -14,6 +14,7 @@ Data Plotting Script
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 df = pd.read_csv("flight_computer.csv")
@@ -178,11 +179,73 @@ def plot_state_data():
     plt.title("State Estimation Velocity (Until Apogee)")
     plt.savefig("plots/state_vx.png")
 
+def plot_mach_data():
+
+    # Calculating atmospheric temperature using the US Standard Atmospheric Model 
+    # and the Kalman filter estimated altitude
+    altitude_asl = df["state_est_x"] + 219 # in m, 219m is the elevation of launch site
+    ambient_T = 15.04 - 0.00649*altitude_asl + 273.15 # K
+
     
+    Mach = np.zeros(3500) # Calculating Mach with US Standard Temp 
+    Mach_bad = np.zeros(3500) # Calculating Mach number with Barometer Temp
+    gamma = 1.4
+    R = 287 # J/(kg*K)
+    for i in range(3500):
+        Mach[i] = df["state_est_vx"][i]/np.sqrt(gamma*R*ambient_T[i])
+        Mach_bad[i] = df["state_est_vx"][i]/np.sqrt(gamma*R*(df["temperature"][i] + 273.15))
     
+    # Finding the time at which Mach number first hits 1
+    supersonic_start = tstamp[np.where(np.isclose(Mach, 1.0, rtol=1e-1))[0][0]]
+    print('Approximate time when rocket hits sound barrier =', supersonic_start, 'ms')
+
+    # intialize plotting variables
+    fig = plt.figure(dpi=200)
+
+    # Plotting Ambient Temperature
+    plt.plot(tstamp, ambient_T - 273.15, label="Ambient Temperature (C)")
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Temperature")
+    plt.legend()
+    plt.grid()
+    plt.title("US Standard Atmospheric Model Temperature")
+    plt.savefig("plots/standard_temp_model.png")
+    fig.clear()
+
+    # Plotting Mach Number
+    plt.plot(tstamp[0:3500], Mach, label="Mach with US Standard Temp")
+    plt.plot(tstamp[0:3500], Mach_bad, label="Mach with Barometer Temp")
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Mach Number")
+    plt.legend()
+    plt.grid()
+    plt.title("Mach Number")
+    plt.savefig("plots/mach_number.png")
+    fig.clear()
+
+    # Plotting Mach Number and Barometer Pressure near Transonic Flow
+    fig_2axes, ax_pressure = plt.subplots(dpi=200)
+    ax_mach = ax_pressure.twinx()
+    
+    # plot raw barometer data
+    ax_pressure.plot(tstamp[500:1000], df["pressure"][500:1000], 
+                     label="pressure", color="tab:blue", zorder=10)
+    ax_mach.plot(tstamp[500:1000], Mach[500:1000], 
+                        label="mach number", color="tab:orange", zorder=10)
+    
+    # format and save plot
+    ax_pressure.set_xlabel("Time (ms)")
+    ax_pressure.set_ylabel("Pressure (mbar)", color="tab:blue"); ax_mach.set_ylabel("Mach Number", color="tab:orange")
+    ax_pressure.grid(True)
+    plt.title("Baro Pressure and Mach around Transonic Flow")
+    plt.savefig("plots/pressure_and_mach.png")
+    fig_2axes.clear()
+
+
 if __name__ == '__main__':
     # plot_lowG_data()
     # plot_barometer_data()
     # plot_highG_data()
     # plot_state_data()
-    print(tstamp)
+    plot_mach_data()
+    #print(tstamp)
