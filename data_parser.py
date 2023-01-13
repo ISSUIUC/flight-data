2aeed94724487a3cfd837add6455f244311455b4
sendhil2 <sendhil2@illinois.edu>
2023-01-13 00:25:54 -0600

    comments many, many, comments I didn't explain how Calculate works, but I probably should

--- data_parser.py
+++ data_parser.py
@@ -73,13 +73,28 @@ TOKEN: IDENTIFIER
 %ignore WS
 """
 
+
 class Type(ABC):
+    """
+    An abstract class which represents Types in C++. As such, it stores a size and alignment, which is all we
+    care about. Furthermore, it requires that deriving classes implement a "parse" method, which must extract
+    information from the bytes representing this types into json.
+    """
     def __init__(self, size: int, align: int):
         self.size = size
         self.align = align
 
     @staticmethod
     def _calc(types: list[Type]) -> tuple[int, int]:
+        """
+        Given a list of types, return the total size and the required alignment for that sequence of types.
+
+        Total size is more than just the sum of sizes of the types, since we must account for alignment requirements.
+        Required alignment is just the alignment of the most aligned element in the types.
+
+        :param types: A list of Type instances
+        :return: A tuple of (total size, required alignment)
+        """
         idx = 0
         max_align = 1
         for typ in types:
@@ -93,6 +108,14 @@ class Type(ABC):
 
     @staticmethod
     def _get_parts(data: bytes, types: list[Type]) -> list[bytes]:
+        """
+        Given some bytes and a list of types that the bytes represent, yield a list of bytes, each of which corresponds
+        to a type in the list of types. This function takes into account padding bytes used for alignment and ignores them.
+
+        :param data: Some bytes, which must be exactly as large as the size used to represent `types`
+        :param types: A list of types, whose data is stored by `data`
+        :return: A list of bytes, which correspond to ranges in `data` for each type in `types`
+        """
         idx = 0
         parts = []
         for typ in types:
@@ -155,7 +178,7 @@ class Boolean(Type):
 
 
 class Integer(Type):
-    def __init__(self, size: int, signed: bool=True, align: int = None):
+    def __init__(self, size: int, signed: bool = True, align: int = None):
         super().__init__(size, size if align is None else align)
         if size == 1 and signed:
             self._format = "b"
@@ -191,12 +214,37 @@ class Float(Type):
 
 
 class Template:
+    """
+    Generated whenever a templated struct is encountered. It stores what template parameters it requires (what type and
+    what name they have), as well as the Context it was in and the fields of the struct (as AST).
+    """
     def __init__(self, params: list[tuple[str, str]], fields: list[lark.Tree], ctxt: Context):
+        """
+        :param params: A list of parameter info tuples. The first item of the tuple should be either the string
+        "typename" if it expects a type, or anything else otherwise. The second item of the tuple should be the name the
+        parameter has. So template<typename T> would have a params of [("typename, "T")]
+        :param fields: The fields of the templated struct, preserved in their AST node form. This makes substitution of
+        template parameters really simple. The "field" node of the AST should be passed in.
+        :param ctxt: A clone of the context right before the templated struct was evaluated. It shouldn't have the
+        templated struct in it.
+        """
         self.params = params
         self.fields = fields
         self.ctxt = ctxt
 
     def resolve(self, args: list[lark.Tree], calc: Calculate) -> Struct:
+        """
+        Resolve the template, given a substitution specified by the given args.
+
+        A wrinkle is that template syntax is somewhat ambiguous, so we can't tell if something is supposed to be a type
+        or a constexpr until we know what the template is. So we just pass in the full AST nodes for the arguments
+        and resolve them specially once we figure out whether each argument should be a constexpr or a type. This is why
+        we pass in the Calculator instance, so we can do that "resolve them specially" part.
+
+        :param args: The arguments of the template expansion. Should be either "type" or a "const_expr" AST nodes.
+        :param calc: The Calculator instance in which the template expansion was needed.
+        :return: A Struct representing the result of the template expansion.
+        """
         if len(args) != len(self.params):
             raise Exception()
         resolve_ctxt = self.ctxt.clone()
@@ -216,16 +264,27 @@ class Template:
 
 
 class Context:
+    """
+    A convenience class which binds together the three aspects tracked in a namespace (for our purposes), that being
+    constants, types, and templates.
+    """
     def __init__(self, names: dict[str, int], types: dict[str, Type], templates: dict[str, Template]):
         self.names = names
         self.types = types
         self.templates = templates
 
     def clone(self) -> Context:
+        """
+        Creates a copy of the context, so we can change one without affecting the other.
+        """
         return Context(self.names.copy(), self.types.copy(), self.templates.copy())
 
 
 class Calculate(Interpreter):
+    """
+    The primary workhorse class, it takes in AST nodes and a Context, and outputs into that Context the data types
+    that were defined in the AST nodes.
+    """
     def __init__(self, ctxt: Context):
         self.ctxt = ctxt
         self.names = ctxt.names
@@ -326,6 +385,8 @@ class Calculate(Interpreter):
         return thing
 
 
+# this stuff is global for easier editing, I guess
+# look at main to see how it's used
 BASE_NAMES: dict[str, int] = {}
 BASE_TYPES: dict[str, Type] = {
     'bool': Boolean(),
@@ -338,15 +399,31 @@ BASE_CTXT = Context(BASE_NAMES, BASE_TYPES, BASE_TEMPLATES)
 
 
 def render_bytes(n: int) -> str:
-    endings = ["", " kb", " Mb", " Gb"]
+    """
+    A helper function which makes a number of bytes prettier by appending units.
+
+    For example, if given 22200 bytes, this will display it as 22.20 kb.
+
+    :param n: A number of bytes.
+    :return: The number of bytes, with units (up to Tb) and 4 significant digits.
+    """
+    endings = ["", " kb", " Mb", " Gb", " Tb"]
     end_idx = 0
-    while n >= 1000:
+    while n >= 1000 and end_idx < len(endings):
         n /= 1000
         end_idx += 1
     return f"{n:#.4g}{endings[end_idx]}"
 
 
 def get_arguments():
+    """
+    A helper function which parses the arguments passed to the file.
+
+    Most notably, if the "out" parameter isn't supplied, it defaults
+    to the "raw" path with the suffix changed to ".json".
+
+    :return: An object which has three fields, each of which contains a pathlib.Path: 'header', 'raw', and 'out'.
+    """
     parser = ArgumentParser(prog="data_parser")
     parser.add_argument("header", type=pathlib.Path)
     parser.add_argument("raw", type=pathlib.Path)
@@ -363,35 +440,47 @@ def main():
     args = get_arguments()
 
     parser = Lark(grammar, parser="earley")
+    # open up the header file and read it in
     with args.header.open("r") as f:
         text = f.read()
+    # parse the text into an AST made of lark.Tree and lark.Token
     tree = parser.parse(text)
+    # create the global namespace, containing the default types like `bool` and `int`
     ctxt = BASE_CTXT.clone()
+    # create the base Calculate instance for the global namespace, then run it for the AST given
     calc = Calculate(ctxt)
     calc.visit(tree)
 
-    sensor_data_struct = ctxt.types["sensorDataStruct_t"]
+    # extract the info for the sensorDataStruct_t from the context (since Calculate mutates it as it goes)
+    sensor_data_struct: Type = ctxt.types["sensorDataStruct_t"]
 
+    # open up the raw data file and read it in
     with args.raw.open("rb") as f_da:
         all_data: bytes = f_da.read()
 
-    idx = 0
+    idx = 0  # a counter which keeps track of how far into the raw data we are
+    # open up the output file, which we write to incrementally
     with args.out.open("w") as f_out:
         f_out.write("[")
-        prev = False
+        prev = False  # keeps track of whether this is the first line written, so we know whether to prepend a comma
         while idx < len(all_data):
+            # read in a slice of data equal to the size of sensorDataStruct_t, then increment the counter by that much
             raw = all_data[idx:idx+sensor_data_struct.size]
             idx += sensor_data_struct.size
+            # if there wasn't enough data to read, end the loop
             if len(raw) < sensor_data_struct.size:
                 break
 
+            # use the Type.parse method in order to convert the raw binary to json
             out = sensor_data_struct.parse(raw)
             if prev:
                 f_out.write(",\n")
             else:
                 f_out.write("\n")
                 prev = True
+            # write the json to the output file
             f_out.write(repr(out))
+            # displays the progress
             print(f"\rParsed {render_bytes(idx)}/{render_bytes(len(all_data))} bytes", end="", flush=True)
         f_out.write("\n]")
 
