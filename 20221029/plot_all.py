c37aed47d7d74766cae4a4244d6c13ae34749b78
Parth Shrotri <shrotri.parth@gmail.com>
2023-02-04 18:07:04 -0600

    added GNC pressure investigation plots

--- 20221029/plot_all.py
+++ 20221029/plot_all.py
@@ -13,8 +13,8 @@ Data Plotting Script
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv("flight_computer.csv")
@@ -37,7 +37,7 @@ fsm_names = ["Init",
 
 
 plt.rcParams["font.family"] = "monospace"
-plt.style.use("dark_background")
+# plt.style.use("dark_background")
 # plt.style.use("Solarized_Light2")
 
 def plot_lowG_data():
@@ -179,124 +179,27 @@ def plot_state_data():
     plt.title("State Estimation Velocity (Until Apogee)")
     plt.savefig("plots/state_vx.png")
 
-def plot_mach_data():
-
-    # Calculating atmospheric temperature using the US Standard Atmospheric Model 
-    # and the Kalman filter estimated altitude
-    altitude_asl = df["state_est_x"] + 219 # in m, 219m is the elevation of launch site
-    ambient_T = 15.04 - 0.00649*altitude_asl + 273.15 # K
-
-    
-    Mach = np.zeros(3500) # Calculating Mach with US Standard Temp 
-    Mach_bad = np.zeros(3500) # Calculating Mach number with Barometer Temp
-    gamma = 1.4
-    R = 287 # J/(kg*K)
-    for i in range(3500):
-        Mach[i] = df["state_est_vx"][i]/np.sqrt(gamma*R*ambient_T[i])
-        Mach_bad[i] = df["state_est_vx"][i]/np.sqrt(gamma*R*(df["temperature"][i] + 273.15))
-    
-    # Finding the time at which Mach number first hits 1
-    supersonic_start = tstamp[np.where(np.isclose(Mach, 1.0, rtol=1e-1))[0][0]]
-    print('Approximate time when rocket hits sound barrier =', supersonic_start, 'ms')
-
-    # intialize plotting variables
-    fig = plt.figure(dpi=200)
-
-    # Plotting Ambient Temperature
-    plt.plot(tstamp, ambient_T - 273.15, label="Ambient Temperature (C)")
+def plot_flap_baro_coupling():
+    tstamp_pre_apo = tstamp[250:3350]
+    df_pre_apo = df[250:3350]
+    plt.plot(tstamp_pre_apo, df_pre_apo["pressure"].values, label="Barometric Pressure")
+    plt.title("Time vs Barometric Pressure")
     plt.xlabel("Time (ms)")
-    plt.ylabel("Temperature")
+    plt.ylabel("Pressure (mbar)")
+    plt.axvline(3100, color = 'tab:green' , label="Start of Motor Burn")
+    plt.axvline(6800, color = 'tab:red' ,label="End of Motor Burn")
+    plt.axvline(7800, color = 'orange' ,label="Flap Extension Start")
+    plt.axvline(8800, color = 'blue' ,label="Flap Retraction Start ")
+    plt.axvline(9450, color = 'purple' ,label="Flap Retraction End")
+    plt.axvline(11800, color = 'orange')
+    plt.axvline(31050, color = 'pink', label="Apogee")
     plt.legend()
-    plt.grid()
-    plt.title("US Standard Atmospheric Model Temperature")
-    plt.savefig("plots/standard_temp_model.png")
-    fig.clear()
-
-    # Plotting Mach Number
-    plt.plot(tstamp[0:3500], Mach, label="Mach with US Standard Temp")
-    plt.plot(tstamp[0:3500], Mach_bad, label="Mach with Barometer Temp")
-    plt.xlabel("Time (ms)")
-    plt.ylabel("Mach Number")
-    plt.legend()
-    plt.grid()
-    plt.title("Mach Number")
-    plt.savefig("plots/mach_number.png")
-    fig.clear()
-
-    # Plotting Mach Number and Barometer Pressure near Transonic Flow
-    fig_2axes, ax_pressure = plt.subplots(dpi=200)
-    ax_mach = ax_pressure.twinx()
+    plt.savefig("plots/GNC_Baro_investigation.png")
+    plt.show()
     
-    # plot raw barometer data
-    ax_pressure.plot(tstamp[500:1000], df["pressure"][500:1000], 
-                     label="pressure", color="tab:blue", zorder=10)
-    ax_mach.plot(tstamp[500:1000], Mach[500:1000], 
-                        label="mach number", color="tab:orange", zorder=10)
-    
-    # format and save plot
-    ax_pressure.set_xlabel("Time (ms)")
-    ax_pressure.set_ylabel("Pressure (mbar)", color="tab:blue"); ax_mach.set_ylabel("Mach Number", color="tab:orange")
-    ax_pressure.grid(True)
-    plt.title("Baro Pressure and Mach around Transonic Flow")
-    plt.savefig("plots/pressure_and_mach.png")
-    fig_2axes.clear()
-
-def plot_fsm_with_altitude_data():
-    fig_2axes, fsmState = plt.subplots(dpi=200)
-    altitude = fsmState.twinx()
-    # plot fsm states 
-
-    #indexes for apogee
-    #startIndex, endIndex = 2750, 3500
-
-    #indexes for whole flight
-    #startIndex, endIndex = 0, -1
-
-    #indexes for landed
-    startIndex, endIndex = 4000, -1
-
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state2"][startIndex:endIndex], label = "History Buffer 6")
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state1"][startIndex:endIndex], label = "History Buffer 50")
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state3"][startIndex:endIndex], label = "Kalman")
-    altitude.plot(tstamp[startIndex:endIndex], df["barometer_altitude"][startIndex:endIndex], label = "Altitude", color="tab:orange")
-    
-    # format and save plot
-    fsmState.set_xlabel("Time (ms)"); 
-    fsmState.set_ylabel("FSM State")
-    altitude.set_ylabel("Altitude (m)")
-    fsmState.legend()
-    altitude.legend()
-    plt.title("FSM States Near Landing")
-    plt.savefig("plots/fsm_states_and_altitude_near_landing.png")
-    fig_2axes.clear()
-
-def plot_fsm_with_acceleration_data():
-    fig_2axes, fsmState = plt.subplots(dpi=200)
-    altitude = fsmState.twinx()
-    # plot fsm states 
-    startIndex, endIndex = 3050, 3300
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state2"][startIndex:endIndex], label = "History Buffer 6")
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state1"][startIndex:endIndex], label = "History Buffer 50")
-    fsmState.plot(tstamp[startIndex:endIndex], df["rocket_state3"][startIndex:endIndex], label = "Kalman")
-    altitude.plot(tstamp[startIndex:endIndex], df["highg_az"][startIndex:endIndex], label = "Acceleration", color="tab:orange")
-    
-    # format and save plot
-    fsmState.set_xlabel("Time (ms)"); 
-    fsmState.set_ylabel("FSM State")
-    altitude.set_ylabel("Acceleration (gs)")
-    fsmState.legend()
-    altitude.legend()
-    plt.title("FSM States Near Separation")
-    plt.savefig("plots/fsm_states_and_acceleration.png")
-    fig_2axes.clear()
-
-
 if __name__ == '__main__':
     # plot_lowG_data()
     # plot_barometer_data()
     # plot_highG_data()
     # plot_state_data()
-    #plot_mach_data()
-    plot_fsm_with_altitude_data()
-    #plot_fsm_with_acceleration_data()
-    #print(tstamp)
+    plot_flap_baro_coupling()
